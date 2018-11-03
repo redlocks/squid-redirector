9f6c530e5a88d4ab84f188b991981eeafa7ca663
redlocks <43011758+redlocks@users.noreply.github.com>
2018-11-03 14:55:11 +0500

    Update setup_for_centos.sh

--- setup_for_centos.sh
+++ setup_for_centos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#проверка установленного пайтона, сквида, если нет - установить
 #chmod +x ./setup_for_centos
 path_to_redirector = /usr/local/squid-redirector/
 path_to_rsyslog_conf = /etc/rsyslog.d/
@@ -11,7 +12,9 @@ cp /config.json $path_to_redirector
 cp /rsyslog-redirector.conf $path_to_rsyslog_conf
 cp /logrotate-redirector $path_to_logrotate_conf
 
-echo "url_rewrite_children 4" >> $path_to_squid_conf
+echo "url_rewrite_children 5" >> $path_to_squid_conf
+# Number of instances of the above program that should run concurrently.
+# 5 is good enough but you should go for 10 at least. Anything below 5 would not work properly.
 echo "url_rewrite_program /usr/local/squid-redirector/redirector.py /usr/local/squid-redirector/config.json" >> $path_to_squid_conf
 
 service squid restart
